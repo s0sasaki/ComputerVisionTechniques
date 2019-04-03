5cf2d295ce66470ca105afdcf5c3ee8be99e87ad
s0sasaki <25005001+s0sasaki@users.noreply.github.com>
2019-04-03 00:46:20 -0700

    Add files via upload

---
+++ neural_network/readMNIST.m
@@ -0,0 +1,71 @@
+function [imgs labels] = readMNIST(imgFile, labelFile, readDigits, offset)
+    
+    % Read digits
+    fid = fopen(imgFile, 'r', 'b');
+    header = fread(fid, 1, 'int32');
+    if header ~= 2051
+        error('Invalid image file header');
+    end
+    count = fread(fid, 1, 'int32');
+    if count < readDigits+offset
+        error('Trying to read too many digits');
+    end
+    
+    h = fread(fid, 1, 'int32');
+    w = fread(fid, 1, 'int32');
+    
+    if offset > 0
+        fseek(fid, w*h*offset, 'cof');
+    end
+    
+%     imgs = zeros([h w readDigits]);
+    imgs=zeros(readDigits,h*w);
+    
+    for i=1:readDigits
+        for y=1:h
+%             imgs(y,:,i) = fread(fid, w, 'uint8');
+            imgs(i,(28*(y-1)+1):28*y)=fread(fid,w,'uint8');
+        end
+    end
+    
+    fclose(fid);
+
+    % Read digit labels
+    fid = fopen(labelFile, 'r', 'b');
+    header = fread(fid, 1, 'int32');
+    if header ~= 2049
+        error('Invalid label file header');
+    end
+    count = fread(fid, 1, 'int32');
+    if count < readDigits+offset
+        error('Trying to read too many digits');
+    end
+    if offset > 0
+        fseek(fid, offset, 'cof');
+    end
+    
+    labels = fread(fid, readDigits, 'uint8');
+    fclose(fid);
+    
+    % Calc avg digit and count
+%     imgs = trimDigits(imgs, 4);
+    imgs = normalizePixValue(imgs);
+    %[avg num stddev] = getDigitStats(imgs, labels);
+    
+end
+
+
+function digits = trimDigits(digitsIn, border)
+    dSize = size(digitsIn);
+    digits = zeros([dSize(1)-(border*2) dSize(2)-(border*2) dSize(3)]);
+    for i=1:dSize(3)
+        digits(:,:,i) = digitsIn(border+1:dSize(1)-border, border+1:dSize(2)-border, i);
+    end
+end
+
+function digits = normalizePixValue(digits)
+    digits = double(digits);
+    for i=1:size(digits, 3)
+        digits(:,:,i) = digits(:,:,i)./255.0;
+    end
+end
