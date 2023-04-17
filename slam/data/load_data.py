27f91ba0f50ec2d3eedf58de30f510b8fea9e175
s0sasaki <25005001+s0sasaki@users.noreply.github.com>
2023-04-17 17:44:38 -0500

    Add files via upload

---
+++ slam/data/load_data.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+if __name__ == '__main__':
+  dataset = 20
+  
+  with np.load("Encoders%d.npz"%dataset) as data:
+    encoder_counts = data["counts"] # 4 x n encoder counts
+    encoder_stamps = data["time_stamps"] # encoder time stamps
+
+  with np.load("Hokuyo%d.npz"%dataset) as data:
+    lidar_angle_min = data["angle_min"] # start angle of the scan [rad]
+    lidar_angle_max = data["angle_max"] # end angle of the scan [rad]
+    lidar_angle_increment = data["angle_increment"] # angular distance between measurements [rad]
+    lidar_range_min = data["range_min"] # minimum range value [m]
+    lidar_range_max = data["range_max"] # maximum range value [m]
+    lidar_ranges = data["ranges"]       # range data [m] (Note: values < range_min or > range_max should be discarded)
+    lidar_stamsp = data["time_stamps"]  # acquisition times of the lidar scans
+    
+  with np.load("Imu%d.npz"%dataset) as data:
+    imu_angular_velocity = data["angular_velocity"] # angular velocity in rad/sec
+    imu_linear_acceleration = data["linear_acceleration"] # Accelerations in gs (gravity acceleration scaling)
+    imu_stamps = data["time_stamps"]  # acquisition times of the imu measurements
+  
+  with np.load("Kinect%d.npz"%dataset) as data:
+    disp_stamps = data["disparity_time_stamps"] # acquisition times of the disparity images
+    rgb_stamps = data["rgb_time_stamps"] # acquisition times of the rgb images
+
